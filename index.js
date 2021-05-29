9a34018c55a8e6dde8b25efe1e5f7b197bb59cba
Jaxan <whitedragon99@gk.ru>
2021-05-29 23:00:17 +0500

    Подключение файлов разных типов

--- index.js
+++ index.js
@@ -27,6 +27,18 @@ const server = http.createServer((req, res)=>{
 
     let filePath = path.join(__dirname,'public',req.url == '/'? 'index.html':req.url)
    const ext = path.extname(filePath)
+    let contentType = 'text/html'
+
+    switch(ext){
+        case '.css':
+            contentType ='text/css'
+            break
+        case '.js':
+            contentType ='text/javascript'
+            break
+        default:
+            contentType ='text/html'
+    }
 
     if(!ext){
         filePath +='.html'
@@ -40,14 +52,14 @@ const server = http.createServer((req, res)=>{
                     res.end('Error')
                 } else{
                     res.writeHead(200,{
-                        'Content-Type': 'text/html'
+                        'Content-Type': contentType
                     })
                     res.end(data)
                 }
             })
         } else {
             res.writeHead(200,{
-                'Content-Type': 'text/html'
+                'Content-Type': contentType
             })
             res.end(content)
 
