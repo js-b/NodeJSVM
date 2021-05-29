cd27e5f6b990da9e8a9ec174f6e4eba7edf0e10d
Jaxan <whitedragon99@gk.ru>
2021-05-29 22:25:49 +0500

    Запуск сервера

--- index.js
+++ index.js
@@ -1,6 +1,65 @@
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
 
-const chalk = require('chalk')
-const text = require('./data')
+const server = http.createServer((req, res)=>{
+    // if (req.url === '/'){
+    //     fs.readFile(path.join(__dirname,'public', 'index.html'), (err,data) => {
+    //         if (err) {
+    //             throw err
+    //         }
+    //         res.writeHead(200,{
+    //             'Content-Type':'text/html'
+    //         })
+    //         res.end(data)
+    //     })
+    // } else if (req.url === '/about'){
+    //     fs.readFile(path.join(__dirname,'public', 'about.html'), (err,data) => {
+    //         if (err) {
+    //             throw err
+    //         }
+    //         res.writeHead(200,{
+    //             'Content-Type':'text/html'
+    //         })
+    //         res.end(data)
+    //     })
+    // } 
 
-console.log(chalk.blue(text))
-console.log(__filename)
+    let filePath = path.join(__dirname,'public',req.url == '/'? 'index.html':req.url)
+   const ext = path.extname(filePath)
+
+    if(!ext){
+        filePath +='.html'
+    }
+
+    fs.readFile(filePath,(err,content) =>{
+        if (err){
+            fs.readFile(path.join(__dirname,'public','error.html'),(err,data) =>{
+                if(err){
+                    res.writeHead(500)
+                    res.end('Error')
+                } else{
+                    res.writeHead(200,{
+                        'Content-Type': 'text/html'
+                    })
+                    res.end(data)
+                }
+            })
+        } else {
+            res.writeHead(200,{
+                'Content-Type': 'text/html'
+            })
+            res.end(content)
+
+        }
+    })
+})
+
+
+
+
+
+
+server.listen(3000,() => {
+    console.log('Сервер запущен');
+})
