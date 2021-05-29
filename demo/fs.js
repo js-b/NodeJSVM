9f2c2b34e08e1b0a8cb2fd444863c9a1a35c60ca
Jaxan <whitedragon99@gk.ru>
2021-05-29 17:27:50 +0500

    Создание и редактирование файла

--- demo/fs.js
+++ demo/fs.js
@@ -1,10 +1,25 @@
 // File System
 const fs = require('fs')
 const path = require('path')
-fs.mkdir(path.join(__dirname, 'test'),(err)=>{
+
+// fs.mkdir(path.join(__dirname, 'test'),(err)=>{
+//     if(err){
+//         throw err
+//     }
+//     console.log('Папка создана');
+// })
+
+const filePath = path.join(__dirname,'test','text.txt')
+
+fs.writeFile(filePath, ' Hello NodeJs', err => {
     if(err){
         throw err
     }
-    console.log('Папка создана');
-
+    console.log('file created');
+    fs.appendFile(filePath, '\n Hello NodeJs 2', err => {
+        if(err){
+            throw err
+        }
+        console.log('file again created');
+    })
 })
