f50abcac5a677ebe65ba008d3a6c99a29f4d4668
Jaxan <whitedragon99@gk.ru>
2021-05-29 13:20:14 +0500

    Библиотека path для получения путей

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-//(function(exports, require, modulem, __dirname, __filename){
+
 const chalk = require('chalk')
 const text = require('./data')
 
 console.log(chalk.blue(text))
 console.log(__filename)
-//})
